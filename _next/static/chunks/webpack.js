c0bab989c7b83036e2adcc760c87709f7e76af02
ariyaIT <ariyadigital@protonmail.com>
2020-11-07 17:55:43 -0800

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5906d67ab749fed6ba52";
+/******/ 	var hotCurrentHash = "a2f2766e8b8a0016b2b0";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
