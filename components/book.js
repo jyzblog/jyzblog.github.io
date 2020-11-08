6de72d85054317fe86345cd1e465886a4f51f755
Joey Zhou <josephyzhou@gmail.com>
2020-11-08 15:13:27 -0800

    prettier

--- components/book.js
+++ components/book.js
@@ -1,15 +1,19 @@
-import React from 'react'
+import React from 'react';
 import styles from '../styles/content.module.css';
 
 function book(props) {
-    const hashtags = [...props.hashtags];
-    const hashtagStr = hashtags.map( h => `#${h}`).join(' ');
-    
-    return (
-        <li>
-            <a href={props.address}><span className={styles.title}>{props.title}</span></a> <small>({props.author})</small> <div className={styles.hashtags}>{hashtagStr}</div>
-        </li>
-    )
+  const hashtags = [...props.hashtags];
+  const hashtagStr = hashtags.map((h) => `#${h}`).join(' ');
+
+  return (
+    <li>
+      <a href={props.address} target="_blank" rel="noopener noreferrer">
+        <span className={styles.title}>{props.title}</span>
+      </a>{' '}
+      <small>({props.author})</small>
+      <div className={styles.hashtags}>{hashtagStr}</div>
+    </li>
+  );
 }
 
-export default book
+export default book;
--- pages/index.js
+++ pages/index.js
@@ -4,45 +4,69 @@ import Nav from '../components/nav';
 import styles from '../styles/content.module.css';
 
 function index() {
-  return <Fragment>
-    <Nav />
-    <div className={styles.content}>
-      <h3>My interests:</h3>
-      <ul>
-        <li>Economics</li>
-        <li>Psychology</li>
-        <li>Cryptography</li>
-        <li>Cybersecurity</li>
-        <li>Hard sciences</li>
-      </ul>
+  return (
+    <Fragment>
+      <Nav />
+      <div className={styles.content}>
+        <h3>My interests:</h3>
+        <ul>
+          <li>Economics</li>
+          <li>Psychology</li>
+          <li>Cryptography</li>
+          <li>Cybersecurity</li>
+          <li>Hard sciences</li>
+        </ul>
 
-      <h3>What I've done:</h3>
-      <ul>
-        <li>Advised various federal government agencies on modernization & security.</li>
-        <li>Built matching engines, derivatives products, OTC platforms, credit card gateway, aml/kyc pipelines</li>
-        <li>Dealt with clearing houses, custodians, private banks, shadow banks, dark pools</li>
-        <li>Bluetooth, wifi protocols, nfc for IoT</li>
-        <li>Bitcoin: btcd, p2sh, payment channels</li>
-        <li>Ethereum: core dev in 2014, wrote first node implementation: geth, go-ethereum, cpp-ethereum/aleth</li>
-      </ul>
+        <h3>What I've done:</h3>
+        <ul>
+          <li>
+            Advised various federal government agencies on modernization &
+            security.
+          </li>
+          <li>
+            Built matching engines, derivatives products, OTC platforms, credit
+            card gateway, aml/kyc pipelines
+          </li>
+          <li>
+            Dealt with clearing houses, custodians, private banks, shadow banks,
+            dark pools
+          </li>
+          <li>Bluetooth, wifi protocols, nfc for IoT</li>
+          <li>Bitcoin: btcd, p2sh, payment channels</li>
+          <li>
+            Ethereum: core dev in 2014, wrote first node implementation: geth,
+            go-ethereum, cpp-ethereum/aleth
+          </li>
+        </ul>
 
-      <h3>What I do:</h3>
-      <ul>
-        <li>Golang, Rust, Node.js, Swift, React / React Native</li>
-        <li>Exotic/unusual asset classes/commodities</li>
-        <li>Federal Contracting: Interesting in prime/sub/joint ventures in the areas of modernization, cloud, security</li>
-        <li>Angel investing: interested in niche businesses with persistent founders. Standard SAFE(yc)/KISS(500) possible</li>
-      </ul>
+        <h3>What I do:</h3>
+        <ul>
+          <li>Golang, Rust, Node.js, Swift, React / React Native</li>
+          <li>Exotic/unusual asset classes/commodities</li>
+          <li>
+            Federal Contracting: Interesting in prime/sub/joint ventures in the
+            areas of modernization, cloud, security
+          </li>
+          <li>
+            Angel investing: interested in niche businesses with persistent
+            founders. Standard SAFE(yc)/KISS(500) possible
+          </li>
+        </ul>
 
-      <h3>Social:</h3>
-      <ul>
-        <li><a href="https://linkedin.com/in/josephyzhou">LinkedIn</a></li>
-        <li><a href="https://twitter.com/josephzhou">Twitter</a></li>
-      </ul>
+        <h3>Social:</h3>
+        <ul>
+          <li>
+            <a href="https://linkedin.com/in/josephyzhou">LinkedIn</a>
+          </li>
+          <li>
+            <a href="https://twitter.com/josephzhou">Twitter</a>
+          </li>
+        </ul>
 
-      <p>Thanks for stopping by! - J.Z.</p>
-    </div>
-  </Fragment>
+        <p>Thanks for stopping by! - J.Z.</p>
+      </div>
+    </Fragment>
+  );
 }
 
 export default index;
