20090fc5b9e872db4f985f75ec758006ad8a9179
Joey Zhou <josephyzhou@gmail.com>
2020-11-11 14:45:38 -0800

    replace writings with local markdown files

---
+++ pages/writings/[slug].js
@@ -0,0 +1,46 @@
+import React, { Fragment } from 'react';
+import ReactMarkdown from 'react-markdown/with-html';
+import Nav from '../../components/nav';
+import styles from '../../styles/content.module.css';
+import { getPostBySlug, getAllPosts } from '../../lib/api';
+import markdownToHtml from '../../lib/markdownToHtml';
+
+function Post({ content }) {
+  return (
+    <Fragment>
+      <Nav />
+      <article className={styles.content}>
+        <ReactMarkdown escapeHtml={false} source={content} />
+      </article>
+    </Fragment>
+  );
+}
+
+export default Post;
+
+export async function getStaticProps({ params: { slug } }) {
+  const post = getPostBySlug(slug, ['title', 'date', 'slug', 'content']);
+  const content = await markdownToHtml(post?.content || '');
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  const formattedDate = post.date.toLocaleDateString('en-US', options);
+
+  return {
+    props: {
+      content: `# ${post.title}\n#### ${formattedDate}\n${content}`,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const posts = getAllPosts(['slug']);
+  return {
+    paths: posts.map((posts) => {
+      return {
+        params: {
+          slug: posts.slug,
+        },
+      };
+    }),
+    fallback: false,
+  };
+}
