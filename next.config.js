6afe0d6866fd82417c18a92627b2e1ba45e5f6bd
Joey Zhou <jozhou@mozilla.com>
2026-02-04 12:02:14 -0800

    Update Next.js to version 16.1.6 and add turbopack configuration in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  turbopack: {},
   webpack: (config, { dev }) => {
     config.module.rules.forEach(rule => {
       if (rule.use && rule.use.loader === 'babel-loader') {
