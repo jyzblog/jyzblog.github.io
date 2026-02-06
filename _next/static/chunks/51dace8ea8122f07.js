c792820952957f9349ebaffa1c88d86b5b1a1c14
mansaj <jozhou@mozilla.com>
2026-02-05 22:55:58 -0800

    Updates

--- _next/static/chunks/51dace8ea8122f07.js
+++
@@ -1,7 +0,0 @@
-__turbopack_load_page_chunks__("/books", [
-  "static/chunks/ceb6f1052a11f988.js",
-  "static/chunks/fd3e9318cddcb9da.js",
-  "static/chunks/204e2614e99eb52b.js",
-  "static/chunks/d9f143d788e467b4.css",
-  "static/chunks/turbopack-2f571587f71bc2d6.js"
-])
--- _next/static/chunks/6c9fe3d75e75b182.js
+++
@@ -1,7 +0,0 @@
-__turbopack_load_page_chunks__("/writings", [
-  "static/chunks/37026ec81891a310.js",
-  "static/chunks/fd3e9318cddcb9da.js",
-  "static/chunks/204e2614e99eb52b.js",
-  "static/chunks/d9f143d788e467b4.css",
-  "static/chunks/turbopack-ced65240de165f9c.js"
-])
