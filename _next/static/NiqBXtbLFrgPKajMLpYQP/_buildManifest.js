04e32ef31fd4afcbf4f3d51c886e6adc8f02fe44
Joey Zhou <jozhou@mozilla.com>
2026-03-15 19:02:45 -0700

    Updates

---
+++ _next/static/NiqBXtbLFrgPKajMLpYQP/_buildManifest.js
@@ -0,0 +1,34 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/d2362dc6b8f3058d.js"
+  ],
+  "/404": [
+    "static/chunks/4ad35217d16998c2.js"
+  ],
+  "/_error": [
+    "static/chunks/23d94008a6922e19.js"
+  ],
+  "/books": [
+    "static/chunks/7aae370429a82cd4.js"
+  ],
+  "/writings": [
+    "static/chunks/6c3566264ef74180.js"
+  ],
+  "/writings/[slug]": [
+    "static/chunks/e8aded0d4e1af9f1.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/404",
+    "/_app",
+    "/_error",
+    "/books",
+    "/writings",
+    "/writings/[slug]"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/7aae370429a82cd4.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/books", [
+  "static/chunks/ceb6f1052a11f988.js",
+  "static/chunks/fd3e9318cddcb9da.js",
+  "static/chunks/204e2614e99eb52b.js",
+  "static/chunks/e28c298924340356.css",
+  "static/chunks/turbopack-db60914018af5bcc.js"
+])
